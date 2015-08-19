bd0f4f40176d6f1de50ad4c573be969cad5dad31
Mario R. Castellanos <mario502gt@gmail.com>
2015-08-19 13:06:49 -0600

    Using Less

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var path = require('path'),
 	gulp = require('gulp'),
-	livereload = require('gulp-livereload');
+	livereload = require('gulp-livereload'),
+	less = require('gulp-less'),
+    minifyCSS = require('gulp-minify-css');
 
 var staticFiles = [
 	'./public/js/*.js',
 	'./public/css/*.css',
 	'./*.html'
 ];
+
+gulp.task('less', function () {
+    return gulp.src('./less/style.less')
+		.pipe(less())
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('./public/css'));
+});
+
 gulp.task('livereload', function() {
 	gulp.src(staticFiles)
 		.pipe(livereload());
@@ -14,5 +24,6 @@ gulp.task('livereload', function() {
 
 gulp.task('default', function() {
 	livereload.listen({start: true});
+	gulp.watch('./less/**/*.less', ['less']);
 	gulp.watch(staticFiles, ['livereload']);
 });
