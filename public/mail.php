2c1a6be562eef845a948ecd4cfbd1d19bb7085d3
Mario R. Castellanos <mario502gt@gmail.com>
2016-02-27 20:55:09 -0600

    Fix email.

--- public/mail.php
+++ public/mail.php
@@ -18,7 +18,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'info@digitalconex.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.<br />"."Here are the details:<br />- <strong>Name</strong>: $name <br />- <strong>Email</strong>: $email_address<br />- <strong>Message</strong>: $message";
 // $headers = "From: noreply@digitalconex.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 // $headers .= "Reply-To: $email_address";	
 $name = "DigitalConex";
